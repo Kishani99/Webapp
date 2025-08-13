dd89b8a72d8a70ca48cea556d10c3c50d6fc2557
Kishani99 <kisha99@gmail.com>
2025-08-13 03:11:08 -0700

    Customer dash board servlet

---
+++ webapp/src/main/java/CustomerDashboardServlet.java
@@ -0,0 +1,37 @@
+
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/CustomerDashboardServlet")
+public class CustomerDashboardServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+
+       
+        if (session != null && "customer".equals(session.getAttribute("role"))) {
+         
+            RequestDispatcher rd = request.getRequestDispatcher("customer_dashboard.jsp");
+            rd.forward(request, response);
+        } else {
+          
+            response.sendRedirect("login.jsp");
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
